b1c789c1a16e1ad4bc77aa8cb5f783b5edaeaefd
EricaTess <sanchez.ericat@gmail.com>
2020-07-10 15:47:02 -0700

    added Navbar component and installed react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 
 import React from 'react';
+import { 
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+import Navbar from './components/Navbar'
 
 export default function App() {
     
     return(
-        <p>Yay I did it</p>
+        <div>
+            <Router>
+                <Navbar />
+            </Router>
+        </div>
     )
 }
